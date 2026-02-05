5b2561eace96af17bd120bc39601d486255edbdc
alfredou <alfredouiop@gmail.com>
2026-02-04 23:31:19 -0400

    fix: normalize Navbar imports to match filename casing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import Navbar from '../components/Navbar'
+import Navbar from '../components/navbar'
 //import Footer from '@/components/Footer'
 import dynamic from 'next/dynamic' // Import the dynamic function
 
